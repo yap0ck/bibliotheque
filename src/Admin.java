b445cb57e97025aa3fd1a5e91f8ecf7c68c50f1c
Beukens <beukensgaetan@gmail.com>
2023-09-19 09:01:34 +0200

    creation classes

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,9 @@
-public class Admin {
+public class Admin extends User{
+
+    public Admin(String nom, String prenom, String login, String motdepasse) {
+        super(nom, prenom, login, motdepasse);
+        this.setRole("admin");
+    }
 }
+
+
--- src/Livres.java
+++ src/Livres.java
@@ -1,2 +1,45 @@
 public class Livres {
+    private String titre;
+    private String auteur;
+    private int anneePublication;
+    private int isbn;
+
+    public Livres(String titre, String auteur, int anneePublication, int isbn) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.anneePublication = anneePublication;
+        this.isbn = isbn;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public int getAnneePublication() {
+        return anneePublication;
+    }
+
+    public void setAnneePublication(int anneePublication) {
+        this.anneePublication = anneePublication;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
 }
